3753e6972217d8f0e5b4275ea84908c083b70faf
Sávyo Souza <savyosilv@gmail.com>
2023-09-12 08:18:43 -0500

    more tests

--- tests/endpoints.test.js
+++ tests/endpoints.test.js
@@ -4,22 +4,24 @@ const express = require('express');
 const app = require('../app');
 
 const { login } = require('../controller/authController');
+const user = {
+        login: "62277318620",
+        password: "62277318620"
+};
+const fakeUser = {
+    login:"62277318620",
+    password: "62294518384"
+};
 
 // verificar que parametros posso testar e utilizar para avaliação
 // elaborar testes para todos endpoints
 
 describe('Login', () => {
     it('Autenticação de usuario', async () => {
-        //bcrypt.compare = jest.fn().mockResolveValue(true);
-
         app.post('/auth/login', login);
         const response = await request(app)
         .post('/auth/login')
-        .send({
-            login: "62277318620",
-            password: "62277318620"
-        });
-        //console.log(response);
+        .send(user);
         expect(response.statusCode).toBe(200);
     });
 
@@ -28,37 +30,51 @@ describe('Login', () => {
         app.post('/auth/login', login);
         const response = await request(app)
         .post('/auth/login')
-        .send({
-            login: "62277318620",
-            password: "13816849"
-        });
-        //console.log(response);
+        .send(fakeUser);
         expect(response.statusCode).toBe(400);
-        //await expect(login(user, password)).rejects.toThrow('Credenciais inválidas');
       });
 });
 
 describe('Titles', () => {
-    it('Listagem dos titulos', async () => {
-        //bcrypt.compare = jest.fn().mockResolveValue(true);
-        // posso fazer o seguinte, faço login, dps pego o token e uso pra fazer os tranmites da function
+    it('Buscar todos titulos', async () => {
         app.post('/auth/login', login);
         const response = await request(app)
         .post('/auth/login')
-        .send({
-            login: "62277318620",
-            password: "62277318620"
-        });
+        .send(user);
         const token = response.body.token;
         
         const responseTitle = await request(app)
         .get('/admin/titles')
         .set('Authorization', token);
-//        console.log(token);
-//        console.log(responseTitle);
+        // const data = Object.keys(responseTitle.body);
+        // const quantidadeDeItens = data.length;
+        // console.log(data);
+        // console.log(quantidadeDeItens);
+        //console.log(body);
         expect(responseTitle.statusCode).toBe(200);
     });
 
+    it('Buscar titulo por id', async () => {
+        app.post('/auth/login', login);
+        const response = await request(app)
+        .post('/auth/login')
+        .send(user)
+
+        const token = response.body.token;
+
+        const responseTitle = await request(app)
+        .get('/admin/titles/1')
+        .set('Authorization', token);
+
+        // const data = responseTitle.body;
+        //console.log(data);
+        // console.log(quantidadeDeItens);
+        const data = Object.keys(responseTitle.body);
+        const quantidadeDeItens = data.length;
+        expect(data[0]).toBe('id');
+    });
+
+    // não entendi esse
     it('Listagem não autorizada', async () => {
         const responseTitle = await request(app)
         .get('/admin/titles')
